e0dfa1fe65561e2e6ebe0afc492cca073574344e
VidulaY <59980975+VidulaY@users.noreply.github.com>
2020-12-17 19:55:47 -0800

    Added negative tests

--- starter_code/src/test/java/com/example/demo/controllers/UsercontrollerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/UsercontrollerTest.java
@@ -10,8 +10,9 @@ import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -45,5 +46,50 @@ public class UsercontrollerTest {
         assertEquals(0, user.getId());
         assertEquals("test", user.getUsername());
         assertEquals("thisIsHashed", user.getPassword());
+    }
+
+    @Test
+    public void create_user_error() throws Exception{
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("Password");
+        createUserRequest.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(createUserRequest);
+        assertNotNull(response);
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    @Test
+    public void findUserByIdTest() throws Exception{
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("Password");
+        createUserRequest.setConfirmPassword("Password");
+        final ResponseEntity<User> response = userController.createUser(createUserRequest);
+        User user = response.getBody();
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("test", response.getBody().getUsername());
+    }
+
+    @Test
+    public void findUserByNameTest() throws Exception{
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+        createUserRequest.setUsername("test");
+        createUserRequest.setPassword("Password");
+        createUserRequest.setConfirmPassword("Password");
+        final ResponseEntity<User> response = userController.createUser(createUserRequest);
+        User user = response.getBody();
+        when(userRepository.findByUsername("testUsername")).thenReturn(user);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("test", response.getBody().getUsername());
+    }
+
+    @Test
+    public void findByIdNotErrorTest(){
+        ResponseEntity<User> response = userController.findById(1L);
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
     }
 }
